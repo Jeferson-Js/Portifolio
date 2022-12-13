a4ca820db5b44fd643011467ad8ffcd8e8c22eaf
Jeferson Santos Oliveira <Jeferson Santos Oliveira>
2022-12-13 17:13:36 -0300

    Página de habilidades foi concluido com sucesso.

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,6 +1,18 @@
-const body = document.querySelector('body')
+const body = document.querySelector("body");
+const btnMenu = document.querySelector(".menu-outline");
 
-function toggleMenu(event){
-    event.name == 'menu' ? event.name = 'close': event.name = 'menu'
-    body.classList.toggle('menu-nav-active')
+function toggleMenu(event) {
+    event.name == "menu" ? (event.name = "close") : (event.name = "menu");
+    body.classList.toggle("menu-nav-active");
 }
+
+const navItem = document.querySelectorAll(".nav-item");
+
+navItem.forEach((item) => {
+    item.addEventListener("click", (event) => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active");
+            event.name == "menu" ? (event.name = "close") : (event.name = "menu");
+        }
+    });
+});
